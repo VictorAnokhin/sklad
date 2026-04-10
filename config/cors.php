bc87f50bcbae24b03ba46983282c91a2e2401a66
Victor Anokhin <autoagent.ua@ukr.net>
2026-04-10 22:28:03 +0300

    update cors

--- config/cors.php
+++ config/cors.php
@@ -20,6 +20,8 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
         'http://localhost:8080',
         'http://localhost:3001',
         'https://app.autoagent.in.ua',
