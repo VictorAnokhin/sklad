8f41d4dc70d535795465a785dcb7cf8113932a87
Victor Anokhin <autoagent.ua@ukr.net>
2026-04-18 12:25:18 +0300

    fix 22

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -196,7 +196,7 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect()->route('login');
+        return redirect()->route('micro-business');
     }
 
     public function forgotPassword(Request $request)
